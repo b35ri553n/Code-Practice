ecdbb7dee178046bd8e710fd9cff2ff899ac9974
FaryalAjradh <ajradfariyal@gmail.com>
2020-10-24 22:54:14 +0530

    comment for better understanding

--- Stacks/balancing.cpp
+++ Stacks/balancing.cpp
@@ -8,15 +8,15 @@ bool matching(char a, char b) {
 bool isBalanced(string str) {
     stack <char> s;
     for(int i = 0; i < str.size(); i++) {
-        if(str[i] == '(' || str[i] == '[' || str[i] == '{') 
+        if(str[i] == '(' || str[i] == '[' || str[i] == '{') //push if bracket is opening
             s.push(str[i]);
         else {
-            if(s.empty()) return false;
-            else if (!matching(str[i], s.top())) return false;
-            else s.pop();
+            if(s.empty()) return false; //for cases like )(
+            else if (!matching(str[i], s.top())) return false; //for cases for (()))
+            else s.pop(); //if matching closing bracket is found, pop out opening bracket
         }
     }
-    return (s.empty());
+    return (s.empty()); //for cases for ((())
 }
 
 int main() {
