87d5a9a28f5137b18aa1a66e784463155f387122
FaryalAjradh <ajradfariyal@gmail.com>
2020-10-24 15:04:57 +0530

    Stack using array

--- Stacks/usingarray.cpp
+++ Stacks/usingarray.cpp
@@ -1,127 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
- 
-class Stack{
-private:
-    int size;
-    int top;
-    int* S;
-public:
-    Stack(int size);
-    ~Stack();
-    void push(int x);
-    int pop();
-    int peek(int index);
-    int isFull();
-    int isEmpty();
-    void display();
-    int stackTop();
-};
- 
-Stack::Stack(int size) {
-    this->size = size;
-    top = -1;
-    S = new int[size];
+
+void push(vector <int> &vec, int x, int &top) {
+    top++;
+    vec[top] = x;
 }
- 
-Stack::~Stack() {
-    delete[] S;
+
+int pop(vector <int> &vec, int &top) {
+    int res = vec[top];
+    top--;
+    return res;
 }
- 
-void Stack::push(int x) {
-    if (isFull()){
-        cout << "Stack Overflow!" << endl;
-    } else {
-        top++;
-        S[top] = x;
-    }
-}
- 
-int Stack::pop() {
-    int x = 1;
-    if (isEmpty()){
-        cout << "Stack Underflow!" << endl;
-    } else {
-        x = S[top];
-        top--;
-    }
-    return x;
-}
- 
-int Stack::peek(int index) {
-    int x = -1;
-    if (top-index+1 < 0 || top-index+1 >= size){
-        cout << "Invalid position!" << endl;
-    } else {
-        x = S[top-index+1];
-    }
-    return x;
-}
- 
-int Stack::isFull() {
-    if (top == size-1){
-        return 1;
-    }
-    return 0;
-}
- 
-int Stack::isEmpty() {
-    if (top == -1){
-        return 1;
-    }
-    return 0;
-}
- 
-void Stack::display() {
-    for (int i=top; i>=0; i--){
-        cout << S[i] << " | " << flush;
+void display(vector <int> vec, int &top) {
+    for(int i = 0; i < top; i++) {
+        cout << vec[i] << " ";
     }
     cout << endl;
 }
- 
-int Stack::stackTop() {
-    if (isEmpty()){
-        return -1;
-    }
-    return S[top];
-}
- 
- 
+
 int main() {
- 
-    int A[] = {1, 3, 5, 7, 9};
- 
-    Stack stk(sizeof(A)/sizeof(A[0]));
- 
-    // Populate stack with array elements
-    for (int i=0; i<sizeof(A)/sizeof(A[0]); i++){
-        stk.push(A[i]);
-    }
-    stk.push(11);
- 
-    cout << "Stack full: " << stk.isFull() << endl;
- 
-    // Display stack;
-    cout << "Stack: " << flush;
-    stk.display();
- 
-    // Peek
-    cout << "Peek at 0th: " << stk.peek(0) << endl;
-    cout << "Peek at 3rd: " << stk.peek(3) << endl;
-    cout << "Peek at 10th: " << stk.peek(10) << endl;
- 
-    // Top element
-    cout << "Top element: " << stk.stackTop() << endl;
- 
-    // Pop out elements from stack
-    cout << "Popped out elements: " << flush;
-    for (int i=0; i<sizeof(A)/sizeof(A[0]); i++){
-        cout << stk.pop() << ", " << flush;
-    }
-    cout << endl;
-    stk.pop();
- 
-    cout << "Stack empty: " << stk.isEmpty() << endl;
- 
-    return 0;
+    vector <int> vec;
+    int top = -1;
+    push(vec, 10, top);
+    push(vec, 20, top);
+    push(vec, 30, top);
+    display(vec, top);
+    cout << "Popped: " << pop(vec, top) << endl;
+    push(vec, 40, top);
+    display(vec, top);
 }
