34716a88ecf7bc4c4468fb6962b4b7a72eace3b5
FaryalAjradh <ajradfariyal@gmail.com>
2020-11-03 16:40:24 +0530

    Make class back to struct

--- Trees/traversals.cpp
+++ Trees/traversals.cpp
@@ -1,22 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node {
-	public:
-		int key;
-		Node *left;
-		Node *right;
-	
-		Node(int k) {
-			key = k;
-			left = right = NULL;
-        }
-        void inorder(Node *root);
-        void preorder(Node *root);
-        void postorder(Node *root);
+struct Node {
+    int key;
+    Node *left;
+    Node *right;
+
+    Node(int k) {
+        key = k;
+        left = right = NULL;
+    }
 };
 
-void Node::inorder(Node *root) {
+void inorder(Node *root) {
     if(root) {      //if tree is empty then we cant access its left right
         inorder(root -> left);
         cout << root -> key << " ";
@@ -24,7 +20,7 @@ void Node::inorder(Node *root) {
     }    
 }
 
-void Node::preorder(Node *root) {
+void preorder(Node *root) {
     if(root) {      //if tree is empty then we cant access its left right
         cout << root -> key << " ";
         inorder(root -> left);
@@ -32,7 +28,7 @@ void Node::preorder(Node *root) {
     }    
 }
 
-void Node::postorder(Node *root) {
+void postorder(Node *root) {
     if(root) {      //if tree is empty then we cant access its left right
         inorder(root -> left);
         inorder(root -> right);
@@ -41,12 +37,12 @@ void Node::postorder(Node *root) {
 }
 
 int main() {
-    Node *root = new Node(10);      //Node *root = NULl for a null root
+    Node *root = new Node(10);
     root -> left = new Node(20);
     root -> right = new Node(30);
     root -> left -> left = new Node(40);
-    root -> inorder(root);
-    root -> preorder(root);
-    root -> postorder(root);
+    inorder(root);
+    preorder(root);
+    postorder(root);
     return 0;
 }
