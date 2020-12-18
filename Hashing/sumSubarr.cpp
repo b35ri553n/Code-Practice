1c8371d727210ccf0c4e8898a3e4b89e7b00a1ee
FaryalAjradh <ajradfariyal@gmail.com>
2020-12-19 00:11:27 +0530

    Check if a sub arr with given sum exists

---
+++ Hashing/sumSubarr.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+//Naive approach
+// bool SumSubarr(int *arr, int n, int sum) {
+//     int current_sum = 0;
+//     for(int i = 0;i < n; i++) {
+//         current_sum = 0;
+//         for(int j = i; j < n; j++) {
+//             current_sum += arr[j];
+//             if(current_sum == sum) return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+//check if presum-sum exists or not
+bool SumSubarr(int *arr, int n, int sum) {
+    unordered_set<int> s;
+    int presum = 0;
+    for(int i = 0; i < n; i++) {
+        presum += arr[i];
+        if(presum == sum) return true;  //when sum is arr[0] to arr[i]
+        if(s.find(presum - sum) != s.end())
+            return true;
+        s.insert(presum);
+    }
+    return false;
+}
+
+int main() {
+    int arr[] = {3, 1, 0, 1, 8, 2, 3, 6};
+    int sum = 5;
+    cout << SumSubarr(arr, 8, sum);
+}
