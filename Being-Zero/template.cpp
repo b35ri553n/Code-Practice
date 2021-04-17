4939ece20aad39f9c2145557b96394f4afdf79cc
FaryalAjradh <ajradfariyal@gmail.com>
2021-04-17 16:07:09 +0530

    Template file for cp

---
+++ Being-Zero/template.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin >> t;
+    // cin.ignore();
+    while(t--) {
+
+    }
+
+    return 0;
+}
